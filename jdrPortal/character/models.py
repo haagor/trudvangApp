8ee989a805b889fc6da2b8c33cc22efd566f71d3
haagor <sim.paris06@gmail.com>
2021-07-12 11:32:28 +0200

    first models

--- jdrPortal/character/models.py
+++ jdrPortal/character/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Traits(models.Model):
+	charisma = models.IntegerField(default=0)
+	constitution = models.IntegerField(default=0)
+	dexterity = models.IntegerField(default=0)
+	intelligence = models.IntegerField(default=0)
+	perception = models.IntegerField(default=0)
+	psyche = models.IntegerField(default=0)
+	strength = models.IntegerField(default=0)
+
+class Character(models.Model):
+	name = models.CharField(max_length=200)
+	traits = models.ForeignKey(Traits, on_delete=models.CASCADE)
